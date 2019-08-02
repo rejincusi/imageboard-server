a59661cf0cd4df7c10afc791d90248e1f16bc7c8
Rejin Cusi <rejincusi@gmail.com>
2019-08-02 14:14:17 +0200

    change database url

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
-const DATABASE_URL  = 'postgres://postgres:pass1@localhost:5432/postgres'
+const DATABASE_URL  = process.env.DATABASE_URL || 'postgres://postgres:pass1@localhost:5432/postgres'
+const port = process.env.PORT || 4000
 
 const db = new Sequelize(DATABASE_URL)
 
